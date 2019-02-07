ef27c039848297d15e31e391e580b2de5a226f8b
alpha.barry <alpha-ibrahima1.barry@etu.u-pec.fr>
2019-02-07 20:44:34 +0100

    [feat] Show spots by using Rest Service

---
+++ app/src/main/java/fr/tas/esipe/tasclientmobile/endpoint/RetrofitClientInstance.java
@@ -0,0 +1,20 @@
+package fr.tas.esipe.tasclientmobile.endpoint;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://172.16.0.2:5000";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
