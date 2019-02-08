ad65e2611567a37d60bc3331eddc2e7cc308500d
Thierno <mamyourydiallo@gmail.com>
2019-02-08 01:28:34 +0100

    first test class for bills scope

---
+++ app/src/test/java/fr/tas/tasclientmobile/PdfAllBillsActivityTest.java
@@ -0,0 +1,44 @@
+package fr.tas.tasclientmobile;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import fr.tas.esipe.tasclientmobile.ConnectToRestApi;
+import fr.tas.esipe.tasclientmobile.PdfAllBillsActivity;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ */
+@Ignore
+@RunWith(MockitoJUnitRunner.class)
+public class PdfAllBillsActivityTest {
+
+    private ConnectToRestApi connectToRestApi;
+
+    private PdfAllBillsActivity pdfAllBillsActivity;
+
+    @Before
+    public void setUp(){
+         connectToRestApi = Mockito.mock(ConnectToRestApi.class);
+
+    }
+    @Test
+    public void shouldInitListOfBills() {
+       /* when(connectToRestApi.execute())
+                .thenReturn(i);*/
+        pdfAllBillsActivity.initList();
+    }
+
+    @Test
+    public void shouldDonwloadClientBill() {
+        pdfAllBillsActivity.initViews();
+    }
+}
