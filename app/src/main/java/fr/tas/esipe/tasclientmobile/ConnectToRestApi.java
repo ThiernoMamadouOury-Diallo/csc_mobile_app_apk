346ebb8b8931e0181bf5a978b0c0a636c35687e2
Thierno <mamyourydiallo@gmail.com>
2019-02-08 01:31:06 +0100

    Make call to rest API to get All recent bills urls for connected user

---
+++ app/src/main/java/fr/tas/esipe/tasclientmobile/ConnectToRestApi.java
@@ -0,0 +1,77 @@
+package fr.tas.esipe.tasclientmobile;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ConnectToRestApi extends AsyncTask<String, Integer, String> {
+
+    /**
+     *
+     * @param urls
+     * @return
+     */
+    protected String doInBackground(String... urls) {
+        String result = "";
+        try{
+
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+
+            // expect HTTP 200 OK, so we don't mistakenly save error report
+            // instead of the file
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return "Server returned HTTP " + connection.getResponseCode()
+                        + " " + connection.getResponseMessage();
+            }
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = "", line;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+            result = content;
+            System.out.println("Retour du serveur :"+result);
+            return result;
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (ProtocolException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param progress
+     */
+    protected void onProgressUpdate(Integer... progress) {
+    }
+
+    /**
+     *
+     * @param result
+     */
+    protected void onPostExecute(String result) {
+        // this is executed on the main thread after the process is over
+
+
+
+    }
+}
